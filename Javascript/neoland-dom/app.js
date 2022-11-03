a4144173996520fbb692b81591243852fc4b20a4
victorrocu <victorcl85@msn.com>
2022-11-03 21:58:49 +0100

    iteracion 1

---
+++ Javascript/neoland-dom/app.js
@@ -0,0 +1,13 @@
+const queryBotonShow = document.querySelector(".showme");
+const queryH1Pillado = document.querySelector("#pillado");
+const queryAllP = document.querySelectorAll("p");
+const queryAllPokemons = document.querySelectorAll(".pokemon");
+const elementTestMe = document.querySelectorAll("span[data-function=testMe]");
+
+
+console.log(queryBotonShow);
+console.log(queryH1Pillado);
+console.log(queryAllP);
+console.log(queryAllPokemons);
+console.log(elementTestMe);
+console.log(elementTestMe.item(2));
